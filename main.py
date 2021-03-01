0b6ca2a647fbb44668d7bd8a4ab556869812bec4
pmucharski <pmucharski2k@gmail.com>
2021-03-01 10:18:52 +0100

    pmucharski

--- main.py
+++ main.py
@@ -12,20 +12,26 @@ print(liczba_rzeczywista)
 
 #zad 2
 #Stwórz skrypt kalkulator, w którym wykorzystać wszystkie podstawowe działania arytmetyczne
-a = int(input("Pierwsza liczba: "))
-b = int(input("Druga liczba: "))
-
-dodawanie = a+b
-print(dodawanie)
-
-odejmowanie = b-a
-print(odejmowanie)
-
-mnozenie = a*b
-print(mnozenie)
-
-dzielenie = b/a
-print(dzielenie)
+# #zad2
+# pierwsza_liczba = input("Podaj pierwszą liczbę: ")
+# znak = input("Podaj znak działania: ")
+# druga_liczba = input("Podaj drugą liczbę: ")
+#
+#
+# if znak == "+":
+#     suma = int(pierwsza_liczba) + int(druga_liczba)
+#     print(suma)
+# elif znak == "-":
+#     roznica = int(pierwsza_liczba) - int(druga_liczba)
+#     print(roznica)
+# elif znak == "*":
+#     iloczyn = int(pierwsza_liczba) * int(druga_liczba)
+#     print(iloczyn)
+# elif znak == "/":
+#     iloraz = int(pierwsza_liczba) / int(druga_liczba)
+#     print(iloraz)
+# else:
+#     print("error")
 
 #zad 3
 #Napis skrypt, w którym stworzysz operatory przyrostkowe dla operacji: +, -, *, /, **, %
@@ -48,11 +54,24 @@ print(e)
 
 #zad 4
 # Napisz skrypt, który policzy i wyświetli następujące wyrażenia:
-pow(e, 10)
+
+a = exp(10)
+
+print(a)
+sinus = sin(8)**2
+logarytm = log(5 + sinus) ** (1/6)
+# b = pow(logarytm, 1/6)
+print(logarytm)
+
+c = floor(3.55)
+print(c)
+d = ceil(4.8)
+print(d)
 
 #Zad.5 Zapisz swoje imie i nazwisko w oddzielnych zmiennych wszystkie wielkimi literami.
 # Użyj odpowiedniej metody by wyświetlić je pisane tak, że pierwsza litera jest wielka a poszostałe małe.
 # (trzeba użyć metody capitalize
+
 imie = "patryk"
 nazwisko = "mucharski"
 print(imie.capitalize())
@@ -65,17 +84,19 @@ print(nazwisko.capitalize())
 napis = ('la la la la la')
 print(napis.count('la))'
 #zad.7
+
 napis1 = 'tekst do sprawdzenia'
 print(napis1[1], napis1[len(napis1)-1])
 #zad.8
+
 print(napis.split())
 #Zad.9 Napisz skrypt, w którym zadeklarujesz zmienne typu:
 # string, float i szestnastkowe. Następnie wyświetl je wykorzystując odpowiednie formatowanie.
-string = 'Hello'
-print(_type(string))
 
-float = 2.5
-print(_type(float))
-
-hex = A1
-print(_type(hex))
+napis2 = "Jest dziś poniedziałek"
+print("Jaki mamy dziś dzień? %s " %  (napis2))
+liczba = 5.677
+print("twoja liczba to: %(z1)f" % {'z1': liczba})
+liczba_szesnastkowo = 0xf351
+print(liczba_szesnastkowo)
+print("%x" % (liczba_szesnastkowo))
